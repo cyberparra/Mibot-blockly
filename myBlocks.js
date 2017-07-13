2c4f4064ac1b68676000a25373f081843066f665
cyberparra <cyberparra@users.noreply.github.com>
2017-07-13 18:06:32 +0200

    Add files via upload

--- myBlocks.js
+++ myBlocks.js
@@ -1,7 +1,75 @@
-// Define your custom blocks generated code
+const FORWARD = 2;
+const BACKWARD = 3;
+const RIGHT = 1;
+const LEFT = 0;
+const STOP = 6
 
 Blockly.JavaScript['wait'] = function(block) {
   var seconds = Blockly.JavaScript.valueToCode(block, 'SECONDS', Blockly.JavaScript.ORDER_ATOMIC);
   var code = 'wait('+seconds+');\n';
+  return code;
+};
+
+Blockly.JavaScript['motor_on'] = function(block) {
+  var dropdown_motor = block.getFieldValue('motor');
+  var dropdown_direction = block.getFieldValue('direction');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['motor_off'] = function(block) {
+  var dropdown_motor = block.getFieldValue('motor');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['spin'] = function(block) {
+  var dropdown_direction = block.getFieldValue('direction');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['penup'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['pendown'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['forward'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'sendCommand(FORWARD);\n';
+  return code;
+};
+
+Blockly.JavaScript['backward'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'sendCommand(BACKWARD);\n';
+  return code;
+};
+
+Blockly.JavaScript['left'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'sendCommand(LEFT);\n';
+  return code;
+};
+
+Blockly.JavaScript['right'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'sendCommand(RIGHT);\n';
+  return code;
+};
+
+Blockly.JavaScript['stop'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'sendCommand(STOP);\n';
   return code;
 };
--- myBlocksInit.js
+++ myBlocksInit.js
@@ -1,4 +1,131 @@
-// Define you custom blocks
+Blockly.Blocks['motor_on'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Motor ON")
+        .appendField("motor")
+        .appendField(new Blockly.FieldDropdown([["Left","L"], ["Right","R"]]), "motor")
+        .appendField("direction")
+        .appendField(new Blockly.FieldDropdown([["Left","L"], ["Right","R"], ["Forward","F"], ["Back","B"]]), "direction");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('turn motor ON');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['motor_off'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Motor OFF")
+        .appendField("motor")
+        .appendField(new Blockly.FieldDropdown([["Left","L"], ["Right","R"]]), "motor");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['spin'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("SPIN")
+        .appendField("direction")
+        .appendField(new Blockly.FieldDropdown([["Left","L"], ["Right","R"]]), "direction");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['penup'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("PEN UP");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(90);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['pendown'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("PEN DOWN");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(90);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['forward'] = {
+  init: function() {
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_CENTRE)
+        .appendField("FORWARD");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('move Mibot forward');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['backward'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("BACKWARD");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('move Mibot back');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['left'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("LEFT");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('move Mibot left');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['right'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("RIGHT");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('Move Mibot right');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['stop'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("STOP");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+    this.setTooltip('stop Mibot');
+    this.setHelpUrl('');
+  }
+};
 
 Blockly.Blocks['wait'] = {
   init: function() {
